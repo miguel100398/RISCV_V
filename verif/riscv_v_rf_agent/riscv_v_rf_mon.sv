4aa2f12b2b695374f14241641a213f44f1920407
miguel100398 <miguel_angel_bucio@hotmail.com>
2023-06-11 13:09:46 -0600

    Added semaphore to monitor to process reads before writes

--- verif/riscv_v_rf_agent/riscv_v_rf_mon.sv
+++ verif/riscv_v_rf_agent/riscv_v_rf_mon.sv
@@ -10,6 +10,11 @@ class riscv_v_rf_mon extends riscv_v_base_mon#( .seq_item_in_t   (riscv_v_rf_wr_
                                                 .seq_item_out_t  (riscv_v_rf_rd_seq_item));
   `uvm_component_utils(riscv_v_rf_mon)
 
+  localparam int rd_sem_keys = 1;
+  localparam int wr_sem_keys = 2;
+  //Semaphore to process read transactions before write transactions
+  semaphore rd_sem;
+  
   //Virtual interface
   virtual riscv_v_rf_if vif;
   // new - constructor
@@ -19,27 +24,30 @@ class riscv_v_rf_mon extends riscv_v_base_mon#( .seq_item_in_t   (riscv_v_rf_wr_
 
   function void build_phase(uvm_phase phase);
     super.build_phase(phase);
+    rd_sem = new(rd_sem_keys);
   endfunction: build_phase
 
-  // run phase
   virtual task run_phase(uvm_phase phase);
     super.run_phase(phase);
-  endtask: run_phase 
+  endtask: run_phase
 
   virtual task mon_rtl_in();
     riscv_v_rf_wr_seq_item wr_txn;
     //Monitor write transaction
-    @(posedge vif.clk);
-
+    @(vif.cb_mon);
+    //2 Keys needed, rd proccess will be executed before
+    rd_sem.get(wr_sem_keys);
     if (vif.cb_mon.wr_en) begin
       `uvm_info("RF_MON", "Transaction captured in wr port", UVM_HIGH);
       wr_txn = riscv_v_rf_wr_seq_item::type_id::create("wr_txn", this);
       wr_txn.addr  = vif.cb_mon.wr_addr;
       wr_txn.wr_en = vif.cb_mon.wr_en;
       wr_txn.data  = vif.cb_mon.data_in;
+      //#0;//First send read transactions
       rtl_in_ap.write(wr_txn);
     end
-
+    //Set keys to process rd taks in the next cycle
+    rd_sem.put(rd_sem_keys);
   endtask: mon_rtl_in
 
   virtual task mon_rtl_out();
@@ -47,22 +55,24 @@ class riscv_v_rf_mon extends riscv_v_base_mon#( .seq_item_in_t   (riscv_v_rf_wr_
     riscv_v_rf_rd_seq_item rd_txn_B;
     rd_txn_A = riscv_v_rf_rd_seq_item::type_id::create("rd_txn_A", this);
     rd_txn_B = riscv_v_rf_rd_seq_item::type_id::create("rd_txn_B", this);
-    @(posedge vif.clk);
-
+    @(vif.cb_mon);
+    rd_sem.get(rd_sem_keys);
     //Monitor rd_port_A
     `uvm_info("RF_MON", "Transaction captured in rd port A", UVM_HIGH);
     rd_txn_A.addr = vif.cb_mon.rd_addr_A;
     rd_txn_A.data = vif.cb_mon.data_out_A;
-    rd_txn_A.port = RF_PORT_A;
+    rd_txn_A.port = RF_RD_PORT_A;
     rtl_out_ap.write(rd_txn_A);
 
     //Monitor rd_port_B
     `uvm_info("RF_MON", "Transaction captured in rd port B", UVM_HIGH);
     rd_txn_B.addr = vif.cb_mon.rd_addr_B;
     rd_txn_B.data = vif.cb_mon.data_out_B;
-    rd_txn_B.port = RF_PORT_B;
+    rd_txn_B.port = RF_RD_PORT_B;
     rtl_out_ap.write(rd_txn_B);
 
+    //Set key to unlock write taks
+    rd_sem.put(wr_sem_keys);
   endtask: mon_rtl_out
 
   //Get interface
