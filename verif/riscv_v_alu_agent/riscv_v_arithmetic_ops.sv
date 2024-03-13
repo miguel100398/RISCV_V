271e8349f421c85d1a2c559631d037e92f0ce8c5
miguel100398 <miguel_angel_bucio@hotmail.com>
2024-03-13 01:11:51 -0600

    fixed Xcelium warnings

--- verif/riscv_v_alu_agent/riscv_v_arithmetic_ops.sv
+++ verif/riscv_v_alu_agent/riscv_v_arithmetic_ops.sv
@@ -14,7 +14,6 @@ class riscv_v_arithmetic_ops extends uvm_component;
         super.new(name, parent);
     endfunction: new
 
-    /*
     virtual function void calc_addc(input riscv_v_arithmetic_alu_in_seq_item arithmetic_in_txn, output riscv_v_zf_t zf_exp, output riscv_v_of_t of_exp, output riscv_v_cf_t cf_exp, output riscv_v_wb_data_t arithmetic_exp_result);
         {zf_exp, of_exp, cf_exp} = 3'b000;
         case(arithmetic_in_txn.osize)
@@ -198,6 +197,7 @@ class riscv_v_arithmetic_ops extends uvm_component;
         endcase
     endfunction: calc_add_reduct
 
+    /*
     virtual function void calc_subb(input riscv_v_arithmetic_alu_in_seq_item arithmetic_in_txn, output riscv_v_zf_t zf_exp, output riscv_v_of_t of_exp, output riscv_v_cf_t cf_exp, output riscv_v_wb_data_t arithmetic_exp_result);
         {zf_exp, of_exp, cf_exp} = 3'b000;
         case(arithmetic_in_txn.osize)
