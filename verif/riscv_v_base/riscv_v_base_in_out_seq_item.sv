794e67c96f2197b366d545b1585b2bd1705f7056
miguel100398 <miguel_angel_bucio@hotmail.com>
2024-03-12 23:59:38 -0600

    fixed xcelium warnings

--- verif/riscv_v_base/riscv_v_base_in_out_seq_item.sv
+++ verif/riscv_v_base/riscv_v_base_in_out_seq_item.sv
@@ -7,8 +7,8 @@
 `define __RISCV_V_BASE_IN_OUT_SEQ_ITEM__
 
 class riscv_v_base_in_out_seq_item#(
-    type in_seq_item_t  = riscv_base_seq_item,
-    type out_seq_item_t = riscv_base_seq_item
+    type in_seq_item_t  = riscv_v_base_seq_item,
+    type out_seq_item_t = riscv_v_base_seq_item
 ) extends base_in_out_seq_item#(
     .in_seq_item_t(in_seq_item_t),
     .out_seq_item_t(out_seq_item_t)
